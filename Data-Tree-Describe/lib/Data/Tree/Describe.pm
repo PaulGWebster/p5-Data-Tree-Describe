5aa7588d9b2afccaf8b90e105fe6bc841fb5af5f
Paul G Webster <paul.g.webster@googlemail.com>
2020-09-26 02:37:46 +0100

    Initial commit

---
+++ Data-Tree-Describe/lib/Data/Tree/Describe.pm
@@ -0,0 +1,54 @@
+package Data::Tree::Describe;
+
+=head1 NAME
+
+Data::Tree::Describe - Module abstract placeholder text
+
+=head1 SYNOPSIS
+
+=for comment Brief examples of using the module.
+
+=head1 DESCRIPTION
+
+=for comment The module's description.
+
+=cut
+
+# Internal perl
+use v5.30.0;
+use feature 'say';
+
+# Internal perl modules (core)
+use strict;
+use warnings;
+
+# Internal perl modules (core,recommended)
+use utf8;
+use open qw(:std :utf8);
+use experimental qw(signatures);
+
+# External modules
+use Carp qw(cluck longmess shortmess);
+
+# Version of this software
+our $VERSION = '0.001';
+
+# Primary code block
+
+
+
+=head1 AUTHOR
+
+Paul G Webster <daemon@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2020 by Paul G Webster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
+=cut
+
+1;
