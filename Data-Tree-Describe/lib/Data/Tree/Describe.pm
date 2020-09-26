a6ed316289b55baf11381684e70f5eebdba06edd
Paul G Webster <paul.g.webster@googlemail.com>
2020-09-26 03:07:51 +0100

    Bump version

--- Data-Tree-Describe/lib/Data/Tree/Describe.pm
+++ Data-Tree-Describe/lib/Data/Tree/Describe.pm
@@ -52,7 +52,7 @@ use experimental qw(signatures);
 use Carp qw(cluck longmess shortmess);
 
 # Version of this software
-our $VERSION = '0.002';
+our $VERSION = '0.003';
 
 # Primary code block
 sub new($class,$input = {}) {
